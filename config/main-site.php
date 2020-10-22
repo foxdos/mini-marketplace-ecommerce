032a3c3e0c65e0df729d294317eae1db099d5b04
foxdos <foxdosbd@gmail.com>
2020-10-22 20:52:30 +0600

    shop clear

--- config/main-site.php
+++ config/main-site.php
@@ -47,6 +47,123 @@ function new_product()
 <?php	}
 }
 
+function show_category()
+{
+    $stmt = DBCON()->prepare("SELECT * FROM category WHERE sts = '1'");
+    $stmt->execute();
+    $data = $stmt->fetchAll();
+    foreach ($data as $k) { ?>
+        <div class="product-plr-1">
+            <div class="single-product-wrap">
+                <div class="product-img product-img-border mb-20">
+                    <a href="shop.php?cat=<?= $k["cat_name"]; ?>">
+                        <img src="assets/category/<?= $k["cat_name"]; ?>.png" alt="">
+                    </a>
+                </div>
+                <div class="product-content-categories-2 text-center">
+                    <h5><a href="shop.php?cat=<?= $k["cat_name"]; ?>"><?= $k["cat_name"]; ?></a></h5>
+                </div>
+            </div>
+        </div>
 
+<?php   }
+}
+
+function show_shop()
+{
+    $stmt = DBCON()->prepare("SELECT * FROM products WHERE sts = '1'");
+    $stmt->execute();
+    $data = $stmt->fetchAll();
+    foreach ($data as $k) { ?>
+        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+            <div class="single-product-wrap mb-35">
+                <div class="product-img product-img-zoom mb-15">
+                    <a href="product-details.html">
+                        <img src="assets/products/<?= $k["img"]; ?>.png" alt="">
+                    </a>
+                </div>
+                <div class="product-content-wrap-2 text-center">
+                    <h3><a href="product.php?id=<?= $k["id"]; ?>"><?= $k["title"]; ?></a></h3>
+                    <div class="product-price-2">
+                        <span>৳ <?= $k["sell_price"]; ?></span>
+                    </div>
+                </div>
+                <div class="product-content-wrap-2 product-content-position text-center">
+                    <h3><a href="product.php?id=<?= $k["id"]; ?>"><?= $k["title"]; ?></a></h3>
+                    <div class="product-price-2">
+                        <span>৳ <?= $k["sell_price"]; ?></span>
+                    </div>
+                    <div class="pro-add-to-cart">
+                        <button title="Add to Cart">Add To Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<?php   }
+}
+
+function show_category_base_product()
+{
+    $cstmt = DBCON()->prepare("SELECT * FROM category WHERE sts = '1'");
+    $cstmt->execute();
+    $cdata = $cstmt->fetchAll();
+    foreach ($cdata as $k) { ?>
+        <div class="product-area pb-85">
+            <div class="container">
+                <div class="section-title-5 section-title-5-bg-1 mb-10">
+                    <i class="red icon-screen-desktop"></i>
+                    <h5 class="red"><?= $k["cat_name"]; ?></h5>
+                </div>
+                <div class="row flex-row-reverse">
+                    <div class="col-lg-12">
+                        <div class="tab-content tab-hm6-categories-slider tab-content-mrg-top jump">
+                            <div id="product-9" class="tab-pane active">
+                                <div class="product-slider-active-5">
+                                    <?php 
+                                    $pstmt = DBCON()->prepare("SELECT * FROM products WHERE sts = '1' AND cat = '".$k["id"]."'");
+                                    $pstmt->execute();
+                                    $data = $pstmt->fetchAll();
+                                    foreach ($data as $p) {
+                                    ?>
+
+                                    <div class="product-plr-1">
+                                        <div class="single-product-wrap">
+                                            <div class="product-img product-img-zoom mb-15">
+                                                <a href="product-details.php">
+                                                    <img src="assets/products/<?= $p["img"]; ?>.png" alt="">
+                                                </a>
+                                            </div>
+                                            <div class="product-content-wrap-2 text-center">
+                                                <h3><a href="product-details.php?id=<?= $p["id"]; ?>"><?= $p["title"]; ?></a></h3>
+                                                <div class="product-price-2">
+                                                    <span>৳ <?= $p["sell_price"]; ?></span>
+                                                </div>
+                                            </div>
+                                            <div class="product-content-wrap-2 product-content-wrap-2-modify product-content-position text-center">
+                                                <h3><a href="product-details.php?id=<?= $p["id"]; ?>"><?= $p["title"]; ?></a></h3>
+                                                <div class="product-price-2">
+                                                    <span>৳ <?= $p["sell_price"]; ?></span>
+                                                </div>
+                                                <div class="pro-add-to-cart">
+                                                    <button title="Add to Cart">Add To Cart</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                <?php } ?>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php   }
+}
 
 ?>
--- footer.php
+++ footer.php
@@ -1,103 +1,69 @@
-        <footer class="footer-area bg-gray-4">
-            <div class="footer-top border-bottom-4 pb-55">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="footer-widget mb-40">
-                                <h3 class="footer-title">Quick Shop</h3>
-                                <div class="footer-info-list info-list-50-parcent">
-                                    <ul>
-                                        <li><a href="shop.php">New In</a></li>
-                                        <li><a href="shop.php">T-Shirts</a></li>
-                                        <li><a href="shop.php">Best Seller</a></li>
-                                        <li><a href="shop.php">Shirts</a></li>
-                                        <li><a href="shop.php">Clothing</a></li>
-                                        <li><a href="shop.php">Bags</a></li>
-                                        <li><a href="shop.php">Men</a></li>
-                                        <li><a href="shop.php">Dresses</a></li>
-                                        <li><a href="shop.php">Women</a></li>
-                                        <li><a href="shop.php">Jeans</a></li>
-                                        <li><a href="shop.php">Baby Girl</a></li>
-                                        <li><a href="shop.php">Shorts</a></li>
-                                        <li><a href="shop.php">Baby Boys</a></li>
-                                        <li><a href="shop.php">Blouses & Shirts</a></li>
-                                        <li><a href="shop.php">Accessories</a></li>
-                                        <li><a href="shop.php">Blazers</a></li>
-                                        <li><a href="shop.php">Shoes</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+<div class="subscribe-area bg-gray pt-115 pb-115">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5 col-md-5">
+                        <div class="section-title">
+                            <h2>keep connected</h2>
+                            <p>Get updates by subscribe our weekly newsletter</p>
                         </div>
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-12">
-                            <div class="footer-widget ml-70 mb-40">
-                                <h3 class="footer-title">useful links</h3>
-                                <div class="footer-info-list">
-                                    <ul>
-                                        <li><a href="my-account.php">My Account</a></li>
-                                        <li><a href="wishlist.php">My Wishlish</a></li>
-                                        <li><a href="#">Term & Conditions</a></li>
-                                        <li><a href="#">Privacy Policy</a></li>
-                                        <li><a href="#">Track Order</a></li>
-                                        <li><a href="shop.php">Shop</a></li>
-                                        <li><a href="about-us.php">About Us</a></li>
-                                        <li><a href="#">Returns/Exchange</a></li>
-                                        <li><a href="#">FAQs</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-12 col-12">
-                            <div class="footer-widget mb-40 ">
-                                <h3 class="footer-title">Contact Us</h3>
-                                <div class="contact-info-2">
-                                    <div class="single-contact-info-2">
-                                        <div class="contact-info-2-icon">
-                                            <i class="icon-call-end"></i>
-                                        </div>
-                                        <div class="contact-info-2-content contact-info-2-content-purple">
-                                            <p>Got a question? Call us 24/7</p>
-                                            <h3 class="purple">(365) 8635 56-24-02 </h3>
-                                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-7">
+                        <div id="mc_embed_signup" class="subscribe-form">
+                            <form id="mc-embedded-subscribe-form" class="validate subscribe-form-style" novalidate="" target="_blank" name="mc-embedded-subscribe-form" method="post" action="http://devitems.us11.list-manage.com/subscribe/post?u=6bbb9b6f5827bd842d9640c82&amp;id=05d85f18ef">
+                                <div id="mc_embed_signup_scroll" class="mc-form">
+                                    <input class="email" type="email" required="" placeholder="Enter your email address" name="EMAIL" value="">
+                                    <div class="mc-news" aria-hidden="true">
+                                        <input type="text" value="" tabindex="-1" name="b_6bbb9b6f5827bd842d9640c82_05d85f18ef">
                                     </div>
-                                    <div class="single-contact-info-2">
-                                        <div class="contact-info-2-icon">
-                                            <i class="icon-cursor icons"></i>
-                                        </div>
-                                        <div class="contact-info-2-content">
-                                            <p>268 Orchard St, Mahattan, 12005, CA, United State</p>
-                                        </div>
-                                    </div>
-                                    <div class="single-contact-info-2">
-                                        <div class="contact-info-2-icon">
-                                            <i class="icon-envelope-open "></i>
-                                        </div>
-                                        <div class="contact-info-2-content">
-                                            <p>contact@norda.com</p>
-                                        </div>
+                                    <div class="clear">
+                                        <input id="mc-embedded-subscribe" class="button" type="submit" name="subscribe" value="Subscribe">
                                     </div>
                                 </div>
-                                <div class="social-style-1 social-style-1-font-inc social-style-1-mrg-2">
-                                    <a href="#"><i class="icon-social-twitter"></i></a>
-                                    <a href="#"><i class="icon-social-facebook"></i></a>
-                                    <a href="#"><i class="icon-social-instagram"></i></a>
-                                    <a href="#"><i class="icon-social-youtube"></i></a>
-                                    <a href="#"><i class="icon-social-pinterest"></i></a>
-                                </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="footer-bottom pt-30 pb-30 ">
-                <div class="container">
-                    <div class="row flex-row-reverse">
-                        <div class="col-lg-6 col-md-6">
-                            <div class="payment-img payment-img-right">
-                                <a href="#"><img src="assets/images/icon-img/payment.png" alt=""></a>
+        </div>
+        <footer class="footer-area bg-gray pb-30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="contact-info-wrap">
+                            <div class="footer-logo">
+                                <a href="#"><img src="assets/images/logo/logo.png" alt="logo"></a>
+                            </div>
+                            <div class="single-contact-info">
+                                <span>Our Location</span>
+                                <p>869 General Village Apt. 645, Moorebury, USA</p>
+                            </div>
+                            <div class="single-contact-info">
+                                <span>24/7 hotline:</span>
+                                <p>(+99) 052 128 2399</p>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-6">
-                            <div class="copyright copyright-center">
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="footer-right-wrap">
+                            <div class="footer-menu">
+                                <nav>
+                                    <ul>
+                                        <li><a href="index.html">home</a></li>
+                                        <li><a href="shop.html">Shop</a></li>
+                                        <li><a href="shop.html">Product </a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><a href="blog.html">Blog.</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <div class="social-style-2 social-style-2-mrg">
+                                <a href="#"><i class="social_twitter"></i></a>
+                                <a href="#"><i class="social_facebook"></i></a>
+                                <a href="#"><i class="social_googleplus"></i></a>
+                                <a href="#"><i class="social_instagram"></i></a>
+                                <a href="#"><i class="social_youtube"></i></a>
+                            </div>
+                            <div class="copyright">
                                 <p>Copyright © 2020 HasThemes | <a href="https://hasthemes.com/">Built with <span>Norda</span> by HasThemes</a>.</p>
                             </div>
                         </div>
@@ -105,126 +71,6 @@
                 </div>
             </div>
         </footer>
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-lg-5 col-md-6 col-12 col-sm-12">
-                                <div class="tab-content quickview-big-img">
-                                    <div id="pro-1" class="tab-pane fade show active">
-                                        <img src="assets/images/product/product-1.jpg" alt="">
-                                    </div>
-                                    <div id="pro-2" class="tab-pane fade">
-                                        <img src="assets/images/product/product-3.jpg" alt="">
-                                    </div>
-                                    <div id="pro-3" class="tab-pane fade">
-                                        <img src="assets/images/product/product-6.jpg" alt="">
-                                    </div>
-                                    <div id="pro-4" class="tab-pane fade">
-                                        <img src="assets/images/product/product-3.jpg" alt="">
-                                    </div>
-                                </div>
-                                <div class="quickview-wrap mt-15">
-                                    <div class="quickview-slide-active nav-style-6">
-                                        <a class="active" data-toggle="tab" href="#pro-1"><img src="assets/images/product/quickview-s1.jpg" alt=""></a>
-                                        <a data-toggle="tab" href="#pro-2"><img src="assets/images/product/quickview-s2.jpg" alt=""></a>
-                                        <a data-toggle="tab" href="#pro-3"><img src="assets/images/product/quickview-s3.jpg" alt=""></a>
-                                        <a data-toggle="tab" href="#pro-4"><img src="assets/images/product/quickview-s2.jpg" alt=""></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-7 col-md-6 col-12 col-sm-12">
-                                <div class="product-details-content quickview-content">
-                                    <h2>Simple Black T-Shirt</h2>
-                                    <div class="product-ratting-review-wrap">
-                                        <div class="product-ratting-digit-wrap">
-                                            <div class="product-ratting">
-                                                <i class="icon_star"></i>
-                                                <i class="icon_star"></i>
-                                                <i class="icon_star"></i>
-                                                <i class="icon_star"></i>
-                                                <i class="icon_star"></i>
-                                            </div>
-                                            <div class="product-digit">
-                                                <span>5.0</span>
-                                            </div>
-                                        </div>
-                                        <div class="product-review-order">
-                                            <span>62 Reviews</span>
-                                            <span>242 orders</span>
-                                        </div>
-                                    </div>
-                                    <p>Seamlessly predominate enterprise metrics without performance based process improvements.</p>
-                                    <div class="pro-details-price">
-                                        <span class="new-price">$75.72</span>
-                                        <span class="old-price">$95.72</span>
-                                    </div>
-                                    <div class="pro-details-color-wrap">
-                                        <span>Color:</span>
-                                        <div class="pro-details-color-content">
-                                            <ul>
-                                                <li><a class="dolly" href="#">dolly</a></li>
-                                                <li><a class="white" href="#">white</a></li>
-                                                <li><a class="azalea" href="#">azalea</a></li>
-                                                <li><a class="peach-orange" href="#">Orange</a></li>
-                                                <li><a class="mona-lisa active" href="#">lisa</a></li>
-                                                <li><a class="cupid" href="#">cupid</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="pro-details-size">
-                                        <span>Size:</span>
-                                        <div class="pro-details-size-content">
-                                            <ul>
-                                                <li><a href="#">XS</a></li>
-                                                <li><a href="#">S</a></li>
-                                                <li><a href="#">M</a></li>
-                                                <li><a href="#">L</a></li>
-                                                <li><a href="#">XL</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="pro-details-quality">
-                                        <span>Quantity:</span>
-                                        <div class="cart-plus-minus">
-                                            <input class="cart-plus-minus-box" type="text" name="qtybutton" value="1">
-                                        </div>
-                                    </div>
-                                    <div class="product-details-meta">
-                                        <ul>
-                                            <li><span>Categories:</span> <a href="#">Woman,</a> <a href="#">Dress,</a> <a href="#">T-Shirt</a></li>
-                                            <li><span>Tag: </span> <a href="#">Fashion,</a> <a href="#">Mentone</a> , <a href="#">Texas</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="pro-details-action-wrap">
-                                        <div class="pro-details-add-to-cart">
-                                            <a title="Add to Cart" href="#">Add To Cart </a>
-                                        </div>
-                                        <div class="pro-details-action">
-                                            <a title="Add to Wishlist" href="#"><i class="icon-heart"></i></a>
-                                            <a title="Add to Compare" href="#"><i class="icon-refresh"></i></a>
-                                            <a class="social" title="Social" href="#"><i class="icon-share"></i></a>
-                                            <div class="product-dec-social">
-                                                <a class="facebook" title="Facebook" href="#"><i class="icon-social-facebook"></i></a>
-                                                <a class="twitter" title="Twitter" href="#"><i class="icon-social-twitter"></i></a>
-                                                <a class="instagram" title="Instagram" href="#"><i class="icon-social-instagram"></i></a>
-                                                <a class="pinterest" title="Pinterest" href="#"><i class="icon-social-pinterest"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Modal end -->
     </div>
 
     <!-- All JS is here
