2f33878cfa026e859c30626d595f91d12c8f701a
S. Mahmud Nabi <shakib15-11655@diu.edu.bd>
2020-10-27 13:56:16 +0600

    final

--- header.php
+++ header.php
@@ -7,7 +7,7 @@ require_once 'config/init.php';
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>eCom</title>
+    <title>Norda Marketplace</title>
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
